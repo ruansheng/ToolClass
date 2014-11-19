dd2404240624333d7cce031d5449856570f57421
ruansheng <825307513@qq.com>
2014-11-19 14:32:31 +0800

    add Curl 、jsoncity

--- Curl.class.php
+++ Curl.class.php
@@ -1,1 +1,31 @@
 <?php
+/**
+ * Curl Class
+ * @author ruansheng
+ */
+class Curl {
+	
+	private static $defaultOptions=array(
+			CURLOPT_CONNECTTIMEOUT => 30,  //连接等待时间
+			CURLOPT_TIMEOUT => 60,  //最长请求时间
+			CURLOPT_RETURNTRANSFER => 1,  //文件流的形式返回
+			CURLOPT_FRESH_CONNECT => true  //每次都用新连接代替缓存中的连接
+	);
+	
+	private static $options=array(
+		
+	);
+	
+	public static function get(){
+		
+	}
+
+	public static function post(){
+	
+	}
+	
+	public static function setCookie(){
+	
+	}
+	
+}
