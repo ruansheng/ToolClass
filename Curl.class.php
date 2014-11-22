7bbebec47f048285ffd80542e3cff882ad82f74e
ruansheng <825307513@qq.com>
2014-11-22 22:52:52 +0800

    add test curl

--- Curl.class.php
+++ Curl.class.php
@@ -5,6 +5,8 @@
  */
 class Curl {
 	
+	private static $handler;
+	
 	private static $defaultOptions=array(
 			CURLOPT_CONNECTTIMEOUT => 30,  //连接等待时间
 			CURLOPT_TIMEOUT => 60,  //最长请求时间
@@ -16,16 +18,44 @@ class Curl {
 		
 	);
 	
-	public static function get(){
-		
+	public static function init(){
+		curl_setopt_array(self::$handler,self::$defaultOptions);
+	}
+	
+	/**
+	 * GET 请求
+	 * @param string $url
+	 * @return mixed
+	 */
+	public static function get($url){
+		self::$handler= curl_init();
+		curl_setopt(self::$handler,CURLOPT_URL,$url);
+		self::init();
+		$result = curl_exec(self::$handler);
+		self::close();
+		return $result;
 	}
 
-	public static function post(){
-	
+	/**
+	 * POST 请求
+	 */
+	public static function post($url,$data=array()){
+		self::$handler= curl_init();
+		curl_setopt(self::$handler,CURLOPT_URL,$url);
+		self::init();
+		curl_setopt(self::$handler, CURLOPT_POST, 1);
+		curl_setopt(self::$handler, CURLOPT_POSTFIELDS,$data);
+		$result = curl_exec(self::$handler);
+		self::close();
+		return $result;
 	}
 	
 	public static function setCookie(){
+		
+	}
 	
+	public static function close(){
+		curl_close(self::$handler);
 	}
 	
 }
