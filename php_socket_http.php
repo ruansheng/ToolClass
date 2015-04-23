6efc188703d28a082cf915206f75651f37b0f3ed
ruansheng <825307513@qq.com>
2015-04-23 18:45:45 +0800

    Update php_socket_http.php

--- php_socket_http.php
+++ php_socket_http.php
@@ -1,4 +1,7 @@
 <?php
+/**
+ * php socket 模拟GET 请求
+ * /
 define('HOST','www.baidu.com');
 define('PORT',80);
 
@@ -26,10 +29,6 @@ $sPatternSeperate = '/\r\n\r\n/';
 $arMatchResponsePart = preg_split($sPatternSeperate, $data, 2);
 
 echo $arMatchResponsePart[0];
-
-
-
-
 
 /*
 HTTP/1.1 200 OK Date: Thu, 23 Apr 2015 10:43:23 GMT Content-Type: text/html Content-Length: 14613 Last-Modified: Wed, 03 Sep 2014 02:48:32 GMT Connection: Close Vary: Accept-Encoding Set-Cookie: BAIDUID=2B1E61769FFE882F890568BF389E1C4B:FG=1; expires=Thu, 31-Dec-37 23:55:55 GMT; max-age=2147483647; path=/; domain=.baidu.com Set-Cookie: BIDUPSID=2B1E61769FFE882F890568BF389E1C4B; expires=Thu, 31-Dec-37 23:55:55 GMT; max-age=2147483647; path=/; domain=.baidu.com Set-Cookie: BDSVRTM=0; path=/ P3P: CP=" OTI DSP COR IVA OUR IND COM " Server: BWS/1.1 X-UA-Compatible: IE=Edge,chrome=1 Pragma: no-cache Cache-control: no-cache BDPAGETYPE: 1 BDQID: 0xce9bd12b000005ef BDUSERID: 0 Accept-Ranges: bytes
